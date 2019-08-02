830e8263d255b11ef8d83dc4bb32b1eaf6918426
idodekerobo <48189364+idodekerobo@users.noreply.github.com>
2019-08-02 13:52:30 -0700

    finished core logic
    fixed taking right and wrong guesses. added counter showing how many guesses left. added logic to stop you from guessing a letter more than one time

--- Assignment4/Hangman.java
+++ Assignment4/Hangman.java
@@ -16,21 +16,18 @@ public class Hangman extends ConsoleProgram {
     public void run() {
 		/* You fill this in */
  
-    	anonWord();
-    	while (wrongGuesses < 3) {
+    	setupGame();
+    	while (wrongGuesses < 3 && ((wordState.indexOf('_') != -1))) {
     		takeGuess();
         	makeGuess();
+        	
+        	if (wordState.indexOf('_') == -1) {
+        		println("Congrats! You guessed the word!");
+        	} else if (wrongGuesses >= 3) {
+        		println("Damn, maybe you'll get it next time!");
+        	}
     	}
     	
-    	/*
-    	setupGame();
-    	
-    	while (wrongGuesses <= 3) {
-    		// take user guesses, if letter is correct show it in word and keep track of wrong guesses
-    		makeGuess();
-    	}
-    	*/
-    	// let user know if they won or lost
 	}
     
     // set up game - choose word and return word in ------
@@ -59,14 +56,20 @@ public class Hangman extends ConsoleProgram {
     		if (guessLetter.length() == 1  && 
     				( (guessLetter.charAt(0) >= 65 && guessLetter.charAt(0) <= 90) || 
     						(guessLetter.charAt(0) >= 97 && guessLetter.charAt(0) <= 122)) ) {
-    			break;
+    			guessLetter = guessLetter.toUpperCase();
+    			if (recordOfGuesses.indexOf(guessLetter.charAt(0)) != -1) {
+    				println("You already guessed that letter!");
+        		} else {
+        			break;
+        		}
+    			
     		}
-    		
-    		println("You can only guess one character at a time and it has to be a letter. Try again.");
+    		println("Remember: you can only guess one character at a time and it has to be a letter. Try again.");
     		guessLetter = readLine("Guess your letter: ");
     	}
     	guessLetter = guessLetter.toUpperCase();
 		guessedLetter = guessLetter.charAt(0);
+		recordOfGuesses += guessedLetter;
 		println("This is your guess: " + guessedLetter);
     }
     
@@ -77,7 +80,10 @@ public class Hangman extends ConsoleProgram {
     	
     	// test if the ch is contained in the secret word, if not add 1 to wrong guesses
     	if (secretWord.indexOf(ch) == -1) {
+    		println("That guess isn't in the word!");
+    		println("This is your progress: " + wordState);
     		wrongGuesses +=1;
+    		println("Here's how many guesses you have left: " + (3 - wrongGuesses) );
     	// if it is, then do stuff
     	} else {
     		
@@ -97,35 +103,11 @@ public class Hangman extends ConsoleProgram {
         			newWordState += wordState.charAt(i);
         		}
         	}
+    		wordState = newWordState;
+    		println("Current word: " + newWordState);
     	}
-    	wordState = newWordState;
-    	println("Current word: " + newWordState);
     	//return newWordState;
     }
-    
-    // replace dash with letter you guessed at the current position in above for loop
-    /*
-    private void addGuessToWord(String letter) {
-    	
-    	
-    	
-    	for (int i=0; i < secretWord.length(); i++) {
-    		String wordChar = "" + secretWord.charAt(i);
-    		
-    		// if char equals an underscore go into the next logic block, if it equals anything else do nothing
-    		if (guess.charAt(i) == 95) {
-    			// if letter equals the letter at word add letter else add -
-        		if (letter.equals(wordChar)) {
-        			guess += letter;
-        		} else {
-        			guess += "_";
-        		}
-    		}
-    		
-    	}
-    	println(guess);
-    }
-    */
     
     // instance variables
     private HangmanLexicon hangmanLexicon = new HangmanLexicon();
@@ -133,15 +115,11 @@ public class Hangman extends ConsoleProgram {
     private String secretWord = hangmanLexicon.getWord(3);
     
     // instance variable that will hold the guess as its built out
-    //private static String guess = "";
     private static char guessedLetter;
     private static String wordState = "";
     private static String newWordState = "";
     private static int wrongGuesses = 0;
-
-    
-//    have to figure out how to count guesses only when answers are wrong
-//    have to figure out how to not restart the progress anytime the letter is right
+    private static String recordOfGuesses = "";
     
     
 }
