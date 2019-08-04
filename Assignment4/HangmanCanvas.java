7e4eeb56347d366ed8c63ad8eef99b46aa845892
idodekerobo <48189364+idodekerobo@users.noreply.github.com>
2019-08-03 18:43:03 -0700

    building out public methods
    added switch statement to see which body part to add

--- Assignment4/HangmanCanvas.java
+++ Assignment4/HangmanCanvas.java
@@ -11,6 +11,16 @@ public class HangmanCanvas extends GCanvas {
 /** Resets the display so that only the scaffold appears */
 	public void reset() {
 		/* You fill this in */
+		removeAll();
+		
+		GLine beam = new GLine(SCAFFOLD_WIDTH, upperLeftCorner, SCAFFOLD_WIDTH, (upperLeftCorner + SCAFFOLD_HEIGHT));
+		add(beam);
+		
+		GLine crane = new GLine(SCAFFOLD_WIDTH, upperLeftCorner, SCAFFOLD_WIDTH + BEAM_LENGTH, upperLeftCorner);
+		add(crane);
+		
+		GLine rope = new GLine(SCAFFOLD_WIDTH + BEAM_LENGTH, upperLeftCorner, SCAFFOLD_WIDTH + BEAM_LENGTH, upperLeftCorner + ROPE_LENGTH);
+		add(rope);
 	}
 
 /**
@@ -20,6 +30,14 @@ public class HangmanCanvas extends GCanvas {
  */
 	public void displayWord(String word) {
 		/* You fill this in */
+		
+		wordProgress = new GLabel(Hangman.wordState, SCAFFOLD_WIDTH, SCAFFOLD_HEIGHT + BEAM_LENGTH);
+		wordProgress.setFont("Courier-24");
+		add(wordProgress);
+		
+		GLabel guesses = new GLabel(Hangman.recordOfGuesses, SCAFFOLD_WIDTH, SCAFFOLD_HEIGHT + BEAM_LENGTH + 50);
+		guesses.setFont("Courier-24");
+		add(guesses);
 	}
 
 /**
@@ -30,6 +48,76 @@ public class HangmanCanvas extends GCanvas {
  */
 	public void noteIncorrectGuess(char letter) {
 		/* You fill this in */
+		putBodyTogether(Hangman.wrongGuesses);
+	}
+	
+	private void putBodyTogether(int index) {
+		switch (index) {
+			case 1: drawHead();
+			break;
+			case 2: drawTorso();
+			break;
+			case 3: drawLeftArm();
+			break;
+			case 4: drawRightArm();
+			break;
+			case 5: drawLeftLeg();
+			break;
+			case 6: drawRightLeg();
+			break;
+			case 7: drawLeftFoot();
+			break;
+			case 8: drawRightFoot();
+			break;
+		}
+	}
+	
+	private void drawHead() {
+		GOval head = new GOval(headX, headY, HEAD_RADIUS*2, HEAD_RADIUS*2);
+		add(head);
+	}
+	
+	private void drawTorso() {
+		GLine torso = new GLine(SCAFFOLD_WIDTH + BEAM_LENGTH, headY + HEAD_RADIUS*2, SCAFFOLD_WIDTH + BEAM_LENGTH, headY + HEAD_RADIUS*2 + BODY_LENGTH);
+		add(torso);
+	}
+	
+	private void drawLeftArm() {
+		GLine arm = new GLine(SCAFFOLD_WIDTH + BEAM_LENGTH, headY + HEAD_RADIUS*2 + ROPE_LENGTH*2, SCAFFOLD_WIDTH + BEAM_LENGTH - UPPER_ARM_LENGTH, headY + HEAD_RADIUS*2 + ROPE_LENGTH*2);
+		GLine lowerArm = new GLine(SCAFFOLD_WIDTH + BEAM_LENGTH - UPPER_ARM_LENGTH, headY + HEAD_RADIUS*2 + ROPE_LENGTH*2, SCAFFOLD_WIDTH + BEAM_LENGTH - UPPER_ARM_LENGTH, headY + HEAD_RADIUS*2 + ROPE_LENGTH*2 + LOWER_ARM_LENGTH);
+		add(arm);
+		add(lowerArm);
+	}
+	
+	private void drawRightArm() {
+		GLine arm = new GLine(SCAFFOLD_WIDTH + BEAM_LENGTH, headY + HEAD_RADIUS*2 + ROPE_LENGTH*2, SCAFFOLD_WIDTH + BEAM_LENGTH + UPPER_ARM_LENGTH, headY + HEAD_RADIUS*2 + ROPE_LENGTH*2);
+		GLine lowerArm = new GLine(SCAFFOLD_WIDTH + BEAM_LENGTH + UPPER_ARM_LENGTH, headY + HEAD_RADIUS*2 + ROPE_LENGTH*2, SCAFFOLD_WIDTH + BEAM_LENGTH + UPPER_ARM_LENGTH, headY + HEAD_RADIUS*2 + ROPE_LENGTH*2 + LOWER_ARM_LENGTH);
+		add(arm);
+		add(lowerArm);
+	}
+	
+	private void drawLeftLeg() {
+		GLine leftHip = new GLine(SCAFFOLD_WIDTH + BEAM_LENGTH, headY + HEAD_RADIUS*2 + BODY_LENGTH, SCAFFOLD_WIDTH + BEAM_LENGTH - HIP_WIDTH, headY + HEAD_RADIUS*2 + BODY_LENGTH);
+		GLine leftLowerLeg = new GLine(SCAFFOLD_WIDTH + BEAM_LENGTH - HIP_WIDTH, headY + HEAD_RADIUS*2 + BODY_LENGTH, SCAFFOLD_WIDTH + BEAM_LENGTH - HIP_WIDTH, headY + HEAD_RADIUS*2 + BODY_LENGTH + LEG_LENGTH);
+		add(leftHip);
+		add(leftLowerLeg);
+	}
+	
+	private void drawRightLeg() {
+		GLine rightHip = new GLine(SCAFFOLD_WIDTH + BEAM_LENGTH, headY + HEAD_RADIUS*2 + BODY_LENGTH, SCAFFOLD_WIDTH + BEAM_LENGTH + HIP_WIDTH, headY + HEAD_RADIUS*2 + BODY_LENGTH);
+		GLine rightLowerLeg = new GLine(SCAFFOLD_WIDTH + BEAM_LENGTH + HIP_WIDTH, headY + HEAD_RADIUS*2 + BODY_LENGTH, SCAFFOLD_WIDTH + BEAM_LENGTH + HIP_WIDTH, headY + HEAD_RADIUS*2 + BODY_LENGTH + LEG_LENGTH);
+		add(rightHip);
+		add(rightLowerLeg);
+	}
+	
+	private void drawLeftFoot() {
+		GLine leftFoot = new GLine(SCAFFOLD_WIDTH + BEAM_LENGTH - HIP_WIDTH, headY + HEAD_RADIUS*2 + BODY_LENGTH + LEG_LENGTH, SCAFFOLD_WIDTH + BEAM_LENGTH - HIP_WIDTH - FOOT_LENGTH, headY + HEAD_RADIUS*2 + BODY_LENGTH + LEG_LENGTH);
+		add(leftFoot);
+	}
+	
+	private void drawRightFoot() {
+		GLine rightFoot = new GLine(SCAFFOLD_WIDTH + BEAM_LENGTH + HIP_WIDTH, headY + HEAD_RADIUS*2 + BODY_LENGTH + LEG_LENGTH, SCAFFOLD_WIDTH + BEAM_LENGTH + HIP_WIDTH + FOOT_LENGTH, headY + HEAD_RADIUS*2 + BODY_LENGTH + LEG_LENGTH);
+		add(rightFoot);
 	}
 
 /* Constants for the simple version of the picture (in pixels) */
@@ -44,5 +132,14 @@ public class HangmanCanvas extends GCanvas {
 	private static final int HIP_WIDTH = 36;
 	private static final int LEG_LENGTH = 108;
 	private static final int FOOT_LENGTH = 28;
+	
+	private static final int upperLeftCorner = 50;
+	private static final int SCAFFOLD_WIDTH = 100;
+	
+	private int headX = (SCAFFOLD_WIDTH + BEAM_LENGTH) - HEAD_RADIUS;
+	private int headY = upperLeftCorner + ROPE_LENGTH;
+	
+	
+	public static GLabel wordProgress;
 
 }
