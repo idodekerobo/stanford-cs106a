b01e6b479f5b0c2a4b766be6d423637447f9648a
idodekerobo <48189364+idodekerobo@users.noreply.github.com>
2019-08-09 20:07:09 -0700

    fixed it
    changed structure of how files were read

--- Assignment5/WordCount.java
+++ Assignment5/WordCount.java
@@ -8,76 +8,97 @@ public class WordCount extends ConsoleProgram {
 		wordCount();
 	}
 	
-private BufferedReader wordCount() {
-		
-		BufferedReader rd = null;
+private void wordCount() {
+
 		String fileName = readLine("What is the file name (w/ .ext at the end)? ");
-		try {
-			//open file
-			rd = new BufferedReader(new FileReader(fileName));
-			
-			// do work
-			
-			fileName(rd);
-			totalLines(rd);
-			totalWords(rd);
-			totalChars(rd);
-			
-			//close file
-			rd.close();
-		} catch (IOException ex) {
-			throw new ErrorException(ex);
-		}
-		return rd; 
+		fileName(fileName);
+		println("Lines: " + totalLines(fileName));
+		println("Words: " + totalWords(fileName));
+		totalChars(fileName);
+
 	}
 	
 	
-	private void fileName(BufferedReader rd) {
-		println("File: ");
+	private void fileName(String fileName) {
+		println("File: " + fileName);
 	}
 	
-	private void totalLines(BufferedReader rd) {
+	// you have to open and close the file ea/ time you want to read from it
+	private int totalLines(String fileName) {
 		int numLines = 0;
 		try {
+			// open file
+			BufferedReader rd = new BufferedReader(new FileReader(fileName));
+
+			// do work
 			while (true) {
 				if (rd.readLine() == null) break;
 				numLines++;
 			}
-			println("Lines: " + numLines);
+			
+			rd.close();
 		} catch (IOException ex) {
 			throw new ErrorException(ex);
 		}
+		
+		return numLines;
 	}
-	
-	private void totalWords(BufferedReader rd) {
+
+	// you have to open and close the file ea/ time you want to read from it
+	private int totalWords(String fileName) {
 		int numWords = 0;
 		try {
+			// open file
+			BufferedReader rd = new BufferedReader(new FileReader(fileName));
+			
+			//do work w/ file
 			while (true) {
-				if (rd.readLine() == null) { break; }
 				String line = rd.readLine();
+				if (line == null) break;
 				for (int i = 0; i < line.length(); i++) {
 					if (line.charAt(i) == ' ') {
 						numWords++;
 					}
 				}
 			}
-			println("Words: " + numWords);
+			int addingBackLines = totalLines(fileName);
+			numWords += addingBackLines;
+			
+			//close file
+			rd.close();
 		} catch (IOException ex) {
 			throw new ErrorException(ex);
 		}
+		
+		return numWords;
 	}
 	
-	private void totalChars(BufferedReader rd) {
+	private int totalChars(String fileName) {
 		int numChars = 0;
-		/* try {
+		try {
+			//open file via passed in filename
+			BufferedReader rd = new BufferedReader(new FileReader(fileName));
 			
+			// do work
+			while (true) {
+				String line = rd.readLine();
+				if (line == null) break;
+				for (int i = 0; i < line.length(); i++) {
+					numChars++;
+				}
+				if (line.charAt(line.length()-1) == ',') {
+					numChars--;
+				}
+			}
 			println("Characters: " + numChars);
+			
+			//close file
+			rd.close();
 		} catch (IOException ex) {
 			throw new ErrorException(ex);
-		} */
+		}
+		
+		return numChars;
 	}
-	
-	
-	
-	
+		
 }
