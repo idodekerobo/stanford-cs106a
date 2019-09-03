13c78a9d8f3f21c3c11ebf489a22a9ac540d370f
idodekerobo <48189364+idodekerobo@users.noreply.github.com>
2019-09-02 18:45:41 -0700

    Create NameSurferDataBase.java

---
+++ Assignment6/NameSurferDataBase.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+import acm.util.*;
+
+/*
+ * File: NameSurferDataBase.java
+ * -----------------------------
+ * This class keeps track of the complete database of names.
+ * The constructor reads in the database from a file, and
+ * the only public method makes it possible to look up a
+ * name and get back the corresponding NameSurferEntry.
+ * Names are matched independent of case, so that "Eric"
+ * and "ERIC" are the same names.
+ */
+
+public class NameSurferDataBase implements NameSurferConstants {
+	
+/* Constructor: NameSurferDataBase(filename) */
+/**
+ * Creates a new NameSurferDataBase and initializes it using the
+ * data in the specified file.  The constructor throws an error
+ * exception if the requested file does not exist or if an error
+ * occurs as the file is being read.
+ */
+	public NameSurferDataBase(String filename) {
+		// You fill this in //
+		try {
+			BufferedReader rd = new BufferedReader(new FileReader(filename));
+			
+			while (true) {
+				String line = rd.readLine();
+				if (line == null) break;
+				NameSurferEntry entry = new NameSurferEntry(line);
+				nameList.add(entry);
+			}
+			rd.close();
+		} catch (IOException ex) {
+			throw new ErrorException(ex);
+		}
+	}
+	
+/* Method: findEntry(name) */
+/**
+ * Returns the NameSurferEntry associated with this name, if one
+ * exists.  If the name does not appear in the database, this
+ * method returns null.
+ */
+	public NameSurferEntry findEntry(String name) {
+		// You need to turn this stub into a real implementation //
+		if (nameList.contains(name)) {
+			int index = nameList.indexOf(name);
+			return (nameList.get(index));
+		} else {
+			return null;
+		}
+	}
+	
+	// instance variables
+	private ArrayList<NameSurferEntry> nameList;
+}
+
