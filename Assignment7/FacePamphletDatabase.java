fd8eecf33bbde572b2c12d34bd712e808fd3d569
idodekerobo <48189364+idodekerobo@users.noreply.github.com>
2019-09-20 16:11:01 -0700

    Create FacePamphletDatabase.java

---
+++ Assignment7/FacePamphletDatabase.java
@@ -0,0 +1,82 @@
+/*
+ * File: FacePamphletDatabase.java
+ * -------------------------------
+ * This class keeps track of the profiles of all users in the
+ * FacePamphlet application.  Note that profile names are case
+ * sensitive, so that "ALICE" and "alice" are NOT the same name.
+ */
+
+import java.util.*;
+
+public class FacePamphletDatabase implements FacePamphletConstants {
+
+	/** 
+	 * Constructor
+	 * This method takes care of any initialization needed for 
+	 * the database.
+	 */
+	public FacePamphletDatabase() {
+		profileMap = new HashMap<String, FacePamphletProfile>();
+	}
+	
+	
+	/** 
+	 * This method adds the given profile to the database.  If the 
+	 * name associated with the profile is the same as an existing 
+	 * name in the database, the existing profile is replaced by 
+	 * the new profile passed in.
+	 */
+	public void addProfile(FacePamphletProfile profile) {
+		String name = profile.getName();
+		
+		/* adds profile to the map. if the key was previously used, the profile assoc w/ that name is replaced */
+		profileMap.put(name, profile);
+	}
+
+	
+	/** 
+	 * This method returns the profile associated with the given name 
+	 * in the database.  If there is no profile in the database with 
+	 * the given name, the method returns null.
+	 */
+	public FacePamphletProfile getProfile(String name) {
+		FacePamphletProfile requestedProfile = profileMap.get(name);
+		
+		if (requestedProfile != null) {
+			return requestedProfile;
+		} else {
+			return null;
+		}
+	}
+	
+	
+	/** 
+	 * This method removes the profile associated with the given name
+	 * from the database.  It also updates the list of friends of all
+	 * other profiles in the database to make sure that this name is
+	 * removed from the list of friends of any other profile.
+	 * 
+	 * If there is no profile in the database with the given name, then
+	 * the database is unchanged after calling this method.
+	 */
+	public void deleteProfile(String name) {
+		profileMap.remove(name);
+	}
+
+	
+	/** 
+	 * This method returns true if there is a profile in the database 
+	 * that has the given name.  It returns false otherwise.
+	 */
+	public boolean containsProfile(String name) {
+		// You fill this in.  Currently always returns false.
+		if (profileMap.containsKey(name)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/* INSTANCE VARIABLES */ 
+	private HashMap<String, FacePamphletProfile> profileMap;
+}
